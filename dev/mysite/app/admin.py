a11b84f2502bb99aa54f9138d317328e8dd46274
sh0g0fujIta <kanayokokami@yahoo.co.jp>
2023-06-02 21:37:44 +0900

    upgrade

--- dev/mysite/app/admin.py
+++ dev/mysite/app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Toppage
+from .models import Toppage, Bicycle, Part, Color
 
 # Register your models here.
 
 admin.site.register(Toppage)
+admin.site.register(Bicycle)
+admin.site.register(Part)
+admin.site.register(Color)
--- dev/mysite/mysite/settings.py
+++ dev/mysite/mysite/settings.py
@@ -68,8 +68,8 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'mysite.wsgi.application'
+
 
 
 # Database
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bicycle_db',
+        'USER': 'root', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': 'fujita', # 同上。そのパスワード
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
