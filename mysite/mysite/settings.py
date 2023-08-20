068f9d68b5c3f50a0746ad1f7c00b718726f7db9
sh0g0fujIta <kanaykokami@yahoo.co.jp>
2023-08-20 12:01:00 +0900

    settings.pyの変更

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -53,7 +54,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "Template"],
+        'DIRS': [os.path.join(BASE_DIR, "Template")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,19 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 STATICFILES_DIRS = [
-    str(BASE_DIR / "static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 "画像をDjangoで読み込むための設定"
 MEDIA_URL = '/media/'
 
 "画像を保存する先の設定"
-MEDIA_ROOT = BASE_DIR / 'media' #画像をアップロードするフォルダを指定
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #画像をアップロードするフォルダを指定
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
